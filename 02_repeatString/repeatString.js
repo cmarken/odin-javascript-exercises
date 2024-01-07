a948e9e814fae1b5ab4e7f5db6642f71cb345984
Chris Marken <11192801+cmarken@users.noreply.github.com>
2024-01-07 15:31:25 +0100

    Pass test 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, nrOfTimes) {
+    let repeatedStr = "";
+    for (let i = 1; i <= nrOfTimes; i++) {
+        repeatedStr += str;
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
