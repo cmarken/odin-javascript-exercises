44d0d8208e6437289f5f4db8e02f870aeb5f3278
Chris Marken <11192801+cmarken@users.noreply.github.com>
2024-01-07 16:13:01 +0100

    Ex 04: Pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, toBeRemoved) {
+    return array.filter(item => item != toBeRemoved);
 };
 
 // Do not edit below this line
