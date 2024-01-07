c43238c8bc727f5072c03ba219e4fae9907090c5
Chris Marken <11192801+cmarken@users.noreply.github.com>
2024-01-07 20:45:51 +0100

    Ex 4: Passing test 1 and 2.
    Was hard to make it work with multiple conditions in the
    filter function. So moved it outside.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function(array, toBeRemoved) {
-    return array.filter(item => item != toBeRemoved);
-};
+const removeFromArray = function(array, ...toBeRemovedList) {
+    for (const toBeFilteredOut of toBeRemovedList) {
+        array = array.filter(item => item != toBeFilteredOut);
+    }
+/*    const newArray = array.filter(item => {
+        for (const toBeFilteredOut of toBeRemovedList) {
+            if (item != toBeFilteredOut)
+                return true;
+        }
+        return false;
+    });*/
+
+    return array;
+};  
 
 // Do not edit below this line
 module.exports = removeFromArray;
