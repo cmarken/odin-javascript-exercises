ab0d7e478a0ed65b2d5cccfdc896646618ceb488
Chris Marken <11192801+cmarken@users.noreply.github.com>
2024-01-07 15:58:39 +0100

    Ex 03: Returning a string in reverese

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const orgStr = str.split("");
 
+    let reversedStr = "";
+    while(orgStr.length > 0) {    
+        reversedStr += orgStr.pop();
+    }
+
+    return reversedStr;
 };
 
 // Do not edit below this line
